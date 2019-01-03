0ffb8f5cdc1bba43ee54fd8ba495106a90cf8555
yyh123 <yuyunhao1@126.com>
2019-01-04 03:29:53 +0800

    编辑信息

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -50,4 +50,35 @@ router.get('/:id',passport.authenticate("jwt", {session: false}),(req,res) => {
         }) 
 })
 
+// 修改信息
+router.post('/edit/:id', passport.authenticate("jwt", {session: false}),(req,res) => {
+    const profileFields = {};
+    if (req.body.type) profileFields.type = req.body.type;
+    if (req.body.describe) profileFields.describe = req.body.describe;
+    if (req.body.income) profileFields.income = req.body.income;
+    if (req.body.expend) profileFields.expend = req.body.expend;
+    if (req.body.cash) profileFields.cash = req.body.cash;
+    if (req.body.remark) profileFields.type = req.body.remark;
+
+    Profile.findOneAndUpdate(
+        {_id: req.params.id},
+        {$set: profileFields},
+        {new: true}
+    ).then(profile => {
+        res.json(profile);
+    })
+})
+
+//删除信息
+router.delete('/delete/:id',passport.authenticate("jwt", {session: false}),(req,res) => {
+    Profile.findOneAndRemove({_id:req.params.id}).then(profile => {
+        profile.save().then(profile => {
+            res.json(profile);
+        }).catch(err => {
+            res.status(404).json('删除失败'); 
+        })
+    })
+        
+})
+
 module.exports = router;
